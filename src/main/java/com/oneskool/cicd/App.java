92ca2a5b9f480008dc9ba3960af62696171b6dc9
Rajiv Narula <rajiv.narula@gmail.com>
2021-05-28 14:57:07 -0400

    First commit

---
+++ src/main/java/com/oneskool/cicd/App.java
@@ -0,0 +1,13 @@
+package com.oneskool.cicd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
